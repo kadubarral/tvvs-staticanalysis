cdae505515f5e677c69c447735272bbf12260dfe
kadubarral <kadubarral@gmail.com>
2020-11-26 14:45:18 +0000

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# tvvs-staticanalysis
+# TVVS Static Analysis
 FEUP TVVS Static Analysis Lecture
+
+This project has the object to show SonarLint and SonarCloud working with Intellij and Github Actions to code review a small Java H2 example.
+
+## Setup Activities
+Fork this project
+![PR](docs/github-FORK.png)
+
+Clone the project that you forked
+```
+git clone https://github.com/YOUR_NAME/tvvs-staticanalysis.git
+```
+
+Branch
+```
+git checkout main
+git pull upstream main && git push origin main
+git checkout -b upXXXXXXXXX/static-analysis-fix
+```
+
+## Fix the issues
+Work in your machine
+```
+cd tvvs-staticanalysis
+```
+
+Static Test is not about to execute the code, but if you want to run locally you can use maven.
+```
+mvn clean compile exec:java
+```
+
+#### To help your work
+* See the actual report of issues on SonarCloud
+  * https://sonarcloud.io/dashboard?id=brave_brave-core
+* Use SonarLint report on Intellij 
+
+## Send your code
+Commit and push
+```
+git add -A
+git commit -m "fix a lot of issues in this horrible code"
+git push origin upXXXXXXXXX/static-analysis-fix
+```
+
+Create the PR
+![PR](docs/github-PR.png)
+
+### Group Members
+* Kadu Barral
+* Sadra Farish
---
+++ src/main/java/staticanalisys.java
@@ -0,0 +1,96 @@
+import java.net.InetAddress;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.Date;
+
+import org.h2.tools.DeleteDbFiles;
+
+
+public class staticanalisys {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        try {
+
+            // delete the database named 'test' in the user home directory
+            DeleteDbFiles.execute("~", "test", true);
+
+            Class.forName("org.h2.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:h2:~/test");
+            Statement stat = conn.createStatement();
+
+            String servername = InetAddress.getLocalHost().getHostName();
+
+            //create table
+            stat.execute("CREATE TABLE ACTIVITY (ID INTEGER, STARTTIME datetime, ENDTIME datetime, SERVERNAME VARCHAR(200), ACTIVITYNAME VARCHAR(200), PRIMARY KEY (ID))");
+
+            //prepared statement
+            PreparedStatement prep = conn.prepareStatement("INSERT INTO ACTIVITY (ID, STARTTIME, ENDTIME, SERVERNAME, ACTIVITYNAME) VALUES (?,?,?,?,?)");
+
+            //insert 10 row data
+            for (int i = 0; i<10; i++){
+                prep.setLong(1, i);
+                prep.setTimestamp(2, new Timestamp(System.currentTimeMillis()));
+                Thread.sleep(500);
+                prep.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
+                prep.setString(4, servername);
+                prep.setString(5, "Activity-" + i);
+
+                //batch insert
+                prep.addBatch();
+            }
+            conn.setAutoCommit(false);
+            prep.executeBatch();
+            conn.setAutoCommit(true);
+
+
+            ResultSet rst = stat.executeQuery("Select count(*) from ACTIVITY");
+            Double total = 0.0;
+            if (rst.next())
+            {
+                total = rst.getDouble(1);
+            }
+
+            Integer count = 1;
+
+            //query to database
+            try {
+                ResultSet rs = stat.executeQuery("Select STARTTIME, ENDTIME, ACTIVITYNAME, SERVERNAME from ACTIVITY where SERVERNAME = '" + servername + "'");
+                while (rs.next()) {
+
+                    Date start = rs.getTimestamp(1);
+                    Date end = rs.getTimestamp(2);
+                    String activityName = rs.getString(3);
+                    String serverName = rs.getString(4);
+
+                    //print query result to console
+                    System.out.println("activity: " + activityName);
+                    System.out.println("local: " + serverName);
+                    System.out.println("start: " + start);
+                    System.out.println("end: " + end);
+                    System.out.println("% done: " + (count/total)*100);
+                    System.out.println("--------------------------");
+
+                    count++;
+                }
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            //close connection
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
